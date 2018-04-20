ea095d5df31812f2f3659e95e8bb9aa027b84dff
Anthony Townsend <anthony@bitsandatoms.net>
2018-04-19 20:44:09 -0400

    clean up

--- njtsign.py
+++ njtsign.py
@@ -12,18 +12,12 @@ import os, time
 
 def serialfind():
     import platform, os, re
-
-    # 1 figure out platform
     platform_name = platform.system()
     tty = ''
-
     if platform_name == 'Linux':
         tty = '/dev/ttyUSB0'
-
     if platform_name == 'Darwin':
-
         dev_contents = os.listdir('/dev')
-
         for line in dev_contents:
             if "Repleo" in line:
                 tty = ('/dev/' + str(line))
@@ -31,10 +25,10 @@ def serialfind():
                 tty = '/dev/tty.usbserial'
             else:
                 tty = '/dev/null'
-
     return
 
 def WriteFont(lines, effect, speed):
+
     # prepare the bitmap
     class Array:
         def zero_one(self, data):
@@ -56,29 +50,19 @@ def WriteFont(lines, effect, speed):
     # THIS IS BREAKING WHEN THE TEXT IS TOO LONG
     # render message as bitmap
     matrix = font.render_multiline(lines, 16 / 2, {"ignore_shift_h": True, "fixed_width": 96})
-
     if not matrix:
         return False
     text_for_sign = Array().zero_one(matrix)
     typeset = sign.queuepix(height=len(matrix), width=len(matrix[0]), data=text_for_sign);
 
-
-
     # queue and send message
     sign.queuemsg(data="%s" % typeset, effect='hold');
     sign.sendqueue(device=portname)
-
-    # pause and report
     time.sleep(6)
 
 
-
-
-
-# refactor so that 'services' to show are key value pairs
-# python njtsign.py 20546,89 30343,119 30343,85 -f font -z 07307 -w
-# parse them and split them at the comma
-
+# parse services and switches
+# njtsign.py 20546,89 30343,119 30343,85 -f font -w
 
 parser = argparse.ArgumentParser()
 parser.add_argument('services', nargs='+', help='Services defined as bus stop#,route# separated by comma with no space')
@@ -87,103 +71,133 @@ parser.add_argument('-f', '--font', dest='font_type', choices=['text','font'], r
 args = parser.parse_args()
 print args
 
-n=0
+
 # extract the services to display
+n=0
+service_panels=[]
 for service in args.services:
     n=n+1
     stop_id=service.split(",")[0]
     route_id=service.split(",")[1]
+    service_panels.append(n,stop_id,route_id)
     print "service %s is stop %s route %s " % (n,stop_id,route_id)
-
 
 # BREAKPOINT --------------------------------------------------------------------------------------------------------
 sys.exit()
 # BREAKPOINT --------------------------------------------------------------------------------------------------------
 
+now = datetime.now()
+#
+# ITERATE OVER EACH SERVICE TO BUILD A MESSAGE
 
+for panel in service_panels:
+
+
+    # create the url
+    api_key = '0.3003391435305782'
+    arrivals_url = 'http://mybusnow.njtransit.com/bustime/eta/getStopPredictionsETA.jsp?route=%s&stop=%s&key=%s'
+    submit_url = arrivals_url % (route_id, args.stop_id, api_key)
+    print submit_url
+
+
+    try:
+        data = urllib2.urlopen(submit_url).read()
+    except urllib2.HTTPError, e:
+        print 'The server couldn\'t fulfill the request.'
+        print 'Error code: ', e.code
+        sys.exit('Exiting.')
+    except urllib2.URLError, e:
+        print 'We failed to reach a server. (internet down?)'
+        ogm = []
+        ogm.append('offline')
+        WritePlaintext(ogm,'hold',3)
+        # print 'Reason: ', e.reason
+
+        sys.exit('Exiting.')
+    else:
+        pass
+
+    arrivals = []
+
+    e = xml.etree.ElementTree.fromstring(data)
+    for atype in e.findall('pre'):
+        fields = { }
+        for field in atype.getchildren():
+            if field.tag not in fields and hasattr(field, 'text'):
+                if field.text is None:
+                    fields[field.tag] = ''
+                    continue
+                fields[field.tag] = field.text.replace("&nbsp", "")
+
+        arrivals.append(fields)
+
+
+    # format outgoing message (big sign only)
+    #
+    # 8:28am Webster & Congress
+    # 119 3m 6m 19m 85 24m
+    #
+
+    # weather
+    temp_now = get_weather.temp(args.zip)
+
+    line2 = ''
+    bus_format = '%s min '
+
+    #
+    # REFACTOR THIS TO LOOP OVER ALL THE LINES FOR A SINGLE STOP WITH NO LINE DESIGNATED
+    #
+
+    for bus in arrivals:
+        print bus
+        if ';' in bus['pt']: # fix for response of APPROACHING e.g. 0 mins prediction
+            bus['pt'] = '0!'
+        bus_entry = bus_format % (bus['pt'])
+        line2 = line2 + bus_entry
+    line2 = "#" + args.route_id + ' ' + line2
+    ogm = []
+    lines = []
+    line1 = datetime.now().strftime('%a') + ' ' + (datetime.now().strftime('%-I:%M %P')) + ' ' + temp_now
+    lines.append(line1)
+    lines.append(line2)
+    ogm = lines[:2]
+    effect = 'hold'
+    speed=1
+
+
+
+
+
+
+#
+# SEND ALL MESSAGES in QUEUE
+
+#
+# ROTATE: TELL THE SCREEN TO SWITCH MESSAGES (60/n sec each)
+
+
+# --------------------------------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------------------------------
+
+#
 # fetching and parsing data
 # right now shows all buses for a single stop
 # direction is implicit
 
-route_id = 'all'
-if args.route_id > 0:
-    route_id = args.route_id
-now  = datetime.now() 
-api_key = '0.3003391435305782'
-arrivals_url = 'http://mybusnow.njtransit.com/bustime/eta/getStopPredictionsETA.jsp?route=%s&stop=%s&key=%s'
-submit_url = arrivals_url % (route_id,args.stop_id,api_key)
-print submit_url
-
-#
-# new architecture will need to loop the URL fetch for each stop + route combination
-# which have to be specified in unique, explicit pairs
-# e.g. -s 31031 119 -s 31031 85 -s 31285 87
-#
 
 
-try:
-    data = urllib2.urlopen(submit_url).read()
-except urllib2.HTTPError, e:
-    print 'The server couldn\'t fulfill the request.'
-    print 'Error code: ', e.code
-    sys.exit('Exiting.')
-except urllib2.URLError, e:
-    print 'We failed to reach a server. (internet down?)'
-    ogm = []
-    ogm.append('offline')
-    WritePlaintext(ogm,'hold',3)
-    # print 'Reason: ', e.reason
-
-    sys.exit('Exiting.')
-else:
-    pass
-
-arrivals = []
-
-e = xml.etree.ElementTree.fromstring(data)
-for atype in e.findall('pre'):
-    fields = { }
-    for field in atype.getchildren():
-        if field.tag not in fields and hasattr(field, 'text'):
-            if field.text is None:
-                fields[field.tag] = ''
-                continue
-            fields[field.tag] = field.text.replace("&nbsp", "")
-
-    arrivals.append(fields)
 
 
-# format outgoing message (big sign only)
-#
-# 8:28am Webster & Congress
-# 119 3m 6m 19m 85 24m
-#
 
-# weather
-temp_now = get_weather.temp(args.zip)
+# route_id = 'all'
+# if args.route_id > 0:
+#    route_id = args.route_id
 
-line2 = ''
-bus_format = '%s min '
 
-#
-# REFACTOR THIS TO LOOP OVER ALL THE LINES FOR A SINGLE STOP WITH NO LINE DESIGNATED
-#
 
-for bus in arrivals:
-    print bus
-    if ';' in bus['pt']: # fix for response of APPROACHING e.g. 0 mins prediction
-        bus['pt'] = '0!'
-    bus_entry = bus_format % (bus['pt'])
-    line2 = line2 + bus_entry
-line2 = "#" + args.route_id + ' ' + line2
-ogm = []
-lines = []
-line1 = datetime.now().strftime('%a') + ' ' + (datetime.now().strftime('%-I:%M %P')) + ' ' + temp_now
-lines.append(line1)
-lines.append(line2)
-ogm = lines[:2]
-effect = 'hold'
-speed=1
 
 # send to LED
 #handle differently depending on render method
