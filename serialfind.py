6e855236aa130cba226d508e6d766b5c87e817ba
Anthony Townsend <anthony@bitsandatoms.net>
2018-04-21 13:27:35 -0400

    deleted serialfind

--- serialfind.py
+++
@@ -1,6 +0,0 @@
-# sets the USB serial port ttyhandle
-# default for linux is /dev/ttyUSB0
-# for OSX, search for a Repleo driver handle
-# otherwise default is /dev/tty.usbserial if it exists
-
-tty
