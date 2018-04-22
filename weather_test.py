1360e54846eb50d27241c67e2cb1151214779c04
Anthony Townsend <anthony@bitsandatoms.net>
2018-04-22 10:44:08 -0400

    revised search code for weather API. need to use place name for now without state

--- weather_test.py
+++ weather_test.py
@@ -2,11 +2,15 @@
 import argparse
 from weather import get_weather
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-z', '--zip', required='True', dest='zip', help="ZIP code of stop, for weather")
-args = parser.parse_args()
+# parser = argparse.ArgumentParser()
+# parser.add_argument('-z', '--zip', required='True', dest='zip', help="ZIP code of stop, for weather")
+# args = parser.parse_args()
 
 
 degree_sign= u'\N{DEGREE SIGN}'
-temp_now = get_weather(args.zip)
-print (temp_now['temp']+degree_sign)
+#theweather= get_weather(args.zip)
+theweather= get_weather('Jersey City')
+print (theweather['temp']+degree_sign)
+print (theweather['temp_lo']+degree_sign)
+print (theweather['temp_hi']+degree_sign)
+print (theweather['status'])
