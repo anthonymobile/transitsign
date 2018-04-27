ac4694b5dbfc15693f858e8efdae78e9933b983f
Anthony Townsend <anthony@bitsandatoms.net>
2018-04-26 21:01:13 -0400

    tested and working on Pi

--- njtsign.py
+++ njtsign.py
@@ -14,7 +14,6 @@ import os, time
 
 def writesign(lines):
 
-    # prepare the bitmap
     class Array:
         def zero_one(self, data):
             zero_oned = ""
@@ -23,15 +22,14 @@ def writesign(lines):
                 zero_oned += joined_row
             return zero_oned
 
-    # font setup
     pwd = os.path.dirname(os.path.realpath(__file__))
     new_glyphs_path = '/'.join([pwd, 'fonts'])
     font = sign_font(new_glyphs_path)
 
-    # sign setup
     portname = '/dev/ttyUSB0'
     sign = MiniSign(devicetype='sign', port=portname, )
 
+    # ------------------------------------------------------------------
     # THIS IS BREAKING WHEN THE TEXT IS TOO LONG
     # render message as bitmap
     matrix = font.render_multiline(lines, 16 / 2, {"ignore_shift_h": True, "fixed_width": 96})
@@ -39,21 +37,20 @@ def writesign(lines):
         return False
     text_for_sign = Array().zero_one(matrix)
     typeset = sign.queuepix(height=len(matrix), width=len(matrix[0]), data=text_for_sign);
+    # 
+    # ------------------------------------------------------------------
 
-    # queue and send message
     sign.queuemsg(data="%s" % typeset, effect='hold');
     sign.sendqueue(device=portname, runslots='none')
 
 
 def main():
 
-    # parse services and switches
     parser = argparse.ArgumentParser()
     parser.add_argument('services', nargs='+', help='Services specified as bus stop#,route# separated by comma with no space')
     parser.add_argument('-w', '--write', dest='write', action='store_true', help="Write the outgoing message (OGM) to the LED screen")
     args = parser.parse_args()
 
-    # extract the service specs
     n=0
     service_specs=[]
     for service in args.services:
@@ -61,16 +58,13 @@ def main():
         stop_id=service.split(",")[0]
         route_id=service.split(",")[1]
         service_specs.append([n,stop_id,route_id])
-        print "service %s is stop %s route %s" % (n,stop_id,route_id)
+        # print "service %s is stop %s route %s" % (n,stop_id,route_id)
 
-    # get arrivals for each service and parse
     slideshow=[]
     for service in service_specs:
-        # create the url
         api_key = '0.3003391435305782'
         arrivals_url = 'http://mybusnow.njtransit.com/bustime/eta/getStopPredictionsETA.jsp?route=%s&stop=%s&key=%s'
         submit_url = arrivals_url % (service[2], service[1], api_key)
-        # print submit_url
 
         try:
             data = urllib2.urlopen(submit_url).read()
@@ -85,6 +79,7 @@ def main():
             pass
         arrival_list = []
         e = xml.etree.ElementTree.fromstring(data)
+
         for atype in e.findall('pre'):
             fields = { }
             for field in atype.getchildren():
@@ -95,11 +90,10 @@ def main():
                     fields[field.tag] = field.text.replace("&nbsp", "")
             arrival_list.append(fields)
 
-        # create slideshow
         line2 = ''
         bus_format = '%s min'
         if len(arrival_list)==0:
-            line2 = 'No scheduled service.'
+            line2 = 'No service.'
             line1 = datetime.now().strftime('%a') + ' ' + (datetime.now().strftime('%-I:%M %P'))  # + ' ' + temp_msg
             lines = []
             lines.append(line1)
@@ -108,11 +102,11 @@ def main():
             slideshow.append(slide)
         else:
             for bus in arrival_list:
-                if bool(bus) is True: # make sure there are predictions
-                    if ';' in bus['pt']:  # handle response of APPROACHING e.g. 0 mins prediction
+                if bool(bus) is True: 
+                    if ';' in bus['pt']:  
                         bus['pt'] = '!0!'
                     bus_entry = bus_format % (bus['pt'])
-                    line2 = line2 + ' ' + bus_entry # append the arrival time for each bus e.g. '22 min'
+                    line2 = line2 + ' ' + bus_entry 
             #degree_sign= u'\N{DEGREE SIGN}'
             #temp_now = get_weather('Jersey City') # hardcoded for now
             #temp_msg = (temp_now['temp']+degree_sign)
@@ -122,26 +116,16 @@ def main():
             lines.append('#' + bus['rd'] + line2)
             slide = lines[:2]
             slideshow.append(slide)
-        print line2
-    print slideshow
+    # print slideshow
 
-    # removes duplicate slides created by above loop if multiple arriving buses
-    # very kludgy need to fix
-    #if len(slideshow)>1:
-    #    slideshow=slideshow[-1]
-    #    print 'Slides: ', slideshow
-    # else:
-    #    pass
 
-    # manually cycle through each message, queue and send it
-    num_slides = len(slideshow)  # type: int
+    num_slides = len(slideshow) 
     slide_duration = 60 / num_slides
     if args.write is True:
-        print 'Writing to sign...'
+        # print 'Writing to sign...'
         for slot in range(len(slideshow)):
-            print str(slot) + ': ',
-            print slideshow[slot]
-            # print 'TESTING SERIAL PORT DISABLED'# writesign(slideshow[slot])
+            print 'Slide ' + str(slot) + ': ',
+            print slideshow[slot],
             writesign(slideshow[slot])
             print 'Sleeping for '+str(slide_duration)+' seconds...'
             time.sleep(slide_duration)
